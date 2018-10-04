40c24adf75213349ca95a9e8fa66416b8ae6b4e0
AbbyB <boekwega@gmail.com>
2018-10-04 23:13:07 +0000

    my message

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // e is the event
-$("#searchButton").click(function(e) {
+$("#searchButton1").click(function(e) {
 
     //log what the event was
     console.log(e);
     //get the value
-    var movieTitle = $("#searchBox").val();
+    var movieTitle = $("#searchBox1").val();
     //log the value
     console.log("user searched for " + movieTitle);
     //the default behavior is to refresh the whole page
@@ -27,11 +27,51 @@ $("#searchButton").click(function(e) {
             console.log(parsed_json);
 
             if (parsed_json['Error'] != null) {
-                $("#movieInfo").html("Error: movie not found");
+                $("#movieInfo1").html("Error: movie not found");
             }
             else {
-                var Rated = "Rated: " + parsed_json['Rated'];
-                $("#movieInfo").html(Rated);
+                var Title = "Title: " + parsed_json['Title'];
+                $("#movieInfo1").html(Title);
+            }
+        }
+    })
+
+});
+
+// e is the event
+$("#searchButton2").click(function(e) {
+
+    //log what the event was
+    console.log(e);
+    //get the value
+    var movieTitle = $("#searchBox2").val();
+    //log the value
+    console.log("user searched for " + movieTitle);
+    //the default behavior is to refresh the whole page
+    e.preventDefault();
+
+    var apiURL = "https://www.omdbapi.com/?apikey=9ee2a57e&t=";
+    apiURL += movieTitle;
+
+    //log the url
+    console.log("the url was " + apiURL);
+
+    $.ajax({
+        url: apiURL,
+        dataType: "json",
+        success: function(parsed_json) {
+
+            var items = parsed_json['items'];
+
+            //log the json return
+            console.log(parsed_json);
+
+            if (parsed_json['Error'] != null) {
+                $("#movieInfo2").html("Error: movie not found");
+            }
+            else {
+                var Title = "Title: " + parsed_json['Title'];
+                $("#movieInfo2").html(Title);
             }
         }
     })

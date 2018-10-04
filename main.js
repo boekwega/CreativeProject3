e4389e63462c8e8f5a92865ae02c0840c2cb066a
AbbyB <boekwega@gmail.com>
2018-10-04 23:03:49 +0000

    stuff, okay

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
 // e is the event
-$("#2009EmmaButton").click(function(e) {
+$("#searchButton").click(function(e) {
 
     //log what the event was
     console.log(e);
     //get the value
-    var value = $("#2009EmmaButton").val();
+    var movieTitle = $("#searchBox").val();
     //log the value
-    console.log(value);
+    console.log("user searched for " + movieTitle);
     //the default behavior is to refresh the whole page
     e.preventDefault();
 
-    //Two ways of displaying the value
-    $("#2009EmmaDisplay").text(value);
-    $("#otherDisplayOption").text(value);
+    var apiURL = "https://www.omdbapi.com/?apikey=9ee2a57e&t=";
+    apiURL += movieTitle;
 
-    var apiURL = "http://www.omdbapi.com/?apikey=9ee2a57e&t=";
-    // apiURL += movieTitle
+    //log the url
+    console.log("the url was " + apiURL);
+
+    $.ajax({
+        url: apiURL,
+        dataType: "json",
+        success: function(parsed_json) {
+
+            var items = parsed_json['items'];
+
+            //log the json return
+            console.log(parsed_json);
+
+            if (parsed_json['Error'] != null) {
+                $("#movieInfo").html("Error: movie not found");
+            }
+            else {
+                var Rated = "Rated: " + parsed_json['Rated'];
+                $("#movieInfo").html(Rated);
+            }
+        }
+    })
 
 });
